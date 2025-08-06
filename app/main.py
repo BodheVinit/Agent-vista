7f3de2402a2cf18c57864c8bc554781596af96e7
Praneeth Kumar <129137379+Praneeth-14@users.noreply.github.com>
2025-08-06 12:05:13 +0530

    Updated main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 # from nodes import followup
 # from nodes import question_Manager
 from app.api.interview_router import router as interview_router
+from app.controllers import flow_controller
 
 app = FastAPI(
     title="Agent-Vista",
@@ -12,3 +13,4 @@ app = FastAPI(
 # app.include_router(followup.router,prefix="/followup",tags=["follow-up"])
 # app.include_router(question_Manager.router,prefix="/questionManager",tags=["questionManager"])
 app.include_router(interview_router, prefix="/interview")
+app.include_router(flow_controller.router,prefix ="/api", tags=["Flow Controller"])
