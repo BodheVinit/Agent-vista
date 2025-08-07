9cb4622c0c428135ea76271609531a226ce6ac03
Vinit Bodhe <vinitbodhe@Vinits-MacBook-Air.local>
2025-08-07 22:14:45 +0530

    web_rtc_video_chat_f

---
+++ server/index.js
@@ -0,0 +1,40 @@
+// server/index.js
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("offer", (data) => {
+    socket.broadcast.emit("offer", data);
+  });
+
+  socket.on("answer", (data) => {
+    socket.broadcast.emit("answer", data);
+  });
+
+  socket.on("ice-candidate", (data) => {
+    socket.broadcast.emit("ice-candidate", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(5001, () => {
+  console.log("Signaling server running on http://localhost:5007");
+});
